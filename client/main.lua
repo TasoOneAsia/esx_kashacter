5f5f2e91041e7bc886705fa0f91974e0e9ee545f
Taso <taso@projecterror.dev>
2021-04-09 12:19:49 -0400

    tweak(client): Improve client performance

--- client/main.lua
+++ client/main.lua
@@ -1,10 +1,42 @@
 local ESX = nil
 local IsChoosing = true
+local cam
+local cam2
+
+
+-- Global to local micro-optimizations
+-- Assigning a global to a local will yield 30% performance gains,
+-- versus traversing _G
+
+local NetworkIsSessionStarted = NetworkIsSessionStarted
+local ShutdownLoadingScreen = ShutdownLoadingScreen
+local DisplayHud = DisplayHud
+local DisplayRadar = DisplayRadar
+local Wait = Wait
+local DoScreenFadeOut = DoScreenFadeOut
+local DoScreenFadeIn = DoScreenFadeIn
+local IsScreenFadedOut = IsScreenFadedOut
+local SetTimecycleModifier = SetTimecycleModifier
+local FreezeEntityPosition = FreezeEntityPosition
+local CreateCamWithParams = CreateCamWithParams
+local SetCamActive = SetCamActive
+local RenderScriptCams = RenderScriptCams
+local SetNuiFocus = SetNuiFocus
+local SendNUIMessage = SendNUIMessage
+local SetEntityCoords = SetEntityCoords
+local PointCamAtCoord = PointCamAtCoord
+local SetCamActiveWithInterp = SetCamActiveWithInterp
+local PlaySoundFrontend = PlaySoundFrontend
+local DestroyCam = DestroyCam
+local TriggerEvent = TriggerEvent
+local TriggerServerEvent = TriggerServerEvent
 
 CreateThread(function()
     while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(0)
+        TriggerEvent('esx:getSharedObject', function(obj)
+            ESX = obj
+        end)
+        Wait(0)
     end
 end)
 
@@ -24,9 +56,9 @@ CreateThread(function()
     end
 end)
 
-CreateThread(function ()
+CreateThread(function()
     while true do
-        Citizen.Wait(0)
+        Wait(0)
         if IsChoosing then
             DisplayHud(false)
             DisplayRadar(false)
@@ -34,19 +66,17 @@ CreateThread(function ()
     end
 end)
 
-local cam, cam2 = nil, nil
-
 RegisterNetEvent('kashactersC:SetupCharacters')
 AddEventHandler('kashactersC:SetupCharacters', function()
     ShutdownLoadingScreen()
-    Citizen.Wait(100)
+    Wait(100)
     DoScreenFadeOut(10)
     while not IsScreenFadedOut() do
-        Citizen.Wait(10)
+        Wait(10)
     end
     SetTimecycleModifier('hud_def_blur')
-    FreezeEntityPosition(GetPlayerPed(-1), true)
-    cam = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", -1355.93,-1487.78,520.75, 300.00,0.00,0.00, 100.00, false, 0)
+    FreezeEntityPosition(PlayerPedId(), true)
+    cam = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", -1355.93, -1487.78, 520.75, 300.00, 0.00, 0.00, 100.00, false, 0)
     SetCamActive(cam, true)
     RenderScriptCams(true, false, 1, true, true)
 end)
@@ -54,7 +84,7 @@ end)
 RegisterNetEvent('kashactersC:SetupUI')
 AddEventHandler('kashactersC:SetupUI', function(Characters)
     DoScreenFadeIn(500)
-    Citizen.Wait(500)
+    Wait(500)
     SetNuiFocus(true, true)
     SendNUIMessage({
         action = "openui",
@@ -65,31 +95,32 @@ end)
 RegisterNetEvent('kashactersC:SpawnCharacter')
 AddEventHandler('kashactersC:SpawnCharacter', function(spawn, isnew)
     local pos = spawn
+    local playerPed = PlayerPedId()
 
     SetTimecycleModifier('default')
-    SetEntityCoords(GetPlayerPed(-1), pos.x, pos.y, pos.z)
+    SetEntityCoords(playerPed, pos.x, pos.y, pos.z)
     DoScreenFadeIn(500)
 
-    Citizen.Wait(500)
+    Wait(500)
 
     cam2 = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", -1355.93,-1487.78,520.75, 300.00,0.00,0.00, 100.00, false, 0)
     PointCamAtCoord(cam2, pos.x,pos.y,pos.z+200)
     SetCamActiveWithInterp(cam2, cam, 900, true, true)
 
-    Citizen.Wait(900)
+    Wait(900)
 
-    cam = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", pos.x,pos.y,pos.z+200, 300.00,0.00,0.00, 100.00, false, 0)
-    PointCamAtCoord(cam, pos.x,pos.y,pos.z+2)
+    cam = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", pos.x, pos.y, pos.z + 200, 300.00, 0.00, 0.00, 100.00, false, 0)
+    PointCamAtCoord(cam, pos.x, pos.y, pos.z + 2)
     SetCamActiveWithInterp(cam, cam2, 3700, true, true)
 
-    Citizen.Wait(3700)
+    Wait(3700)
 
     PlaySoundFrontend(-1, "Zoom_Out", "DLC_HEIST_PLANNING_BOARD_SOUNDS", 1)
     RenderScriptCams(false, true, 500, true, true)
     PlaySoundFrontend(-1, "CAR_BIKE_WHOOSH", "MP_LOBBY_SOUNDS", 1)
-    FreezeEntityPosition(GetPlayerPed(-1), false)
+    FreezeEntityPosition(playerPed, false)
 
-    Citizen.Wait(500)
+    Wait(500)
 
     SetCamActive(cam, false)
     DestroyCam(cam, true)
@@ -111,21 +142,21 @@ AddEventHandler('kashactersC:ReloadCharacters', function()
 end)
 
 RegisterNUICallback("CharacterChosen", function(data, cb)
-    SetNuiFocus(false,false)
+    SetNuiFocus(false, false)
     DoScreenFadeOut(500)
     TriggerServerEvent('kashactersS:CharacterChosen', data.charid, data.ischar)
     while not IsScreenFadedOut() do
-        Citizen.Wait(10)
+        Wait(10)
     end
-    cb({ })
+    cb({})
 end)
 
 RegisterNUICallback("DeleteCharacter", function(data, cb)
-    SetNuiFocus(false,false)
+    SetNuiFocus(false, false)
     DoScreenFadeOut(500)
     TriggerServerEvent('kashactersS:DeleteCharacter', data.charid)
     while not IsScreenFadedOut() do
-        Citizen.Wait(10)
+        Wait(10)
     end
     cb({})
 end)
